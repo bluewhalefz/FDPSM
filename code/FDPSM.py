58e22bb488bf8edc2a5fc2aabba62e5f806e6e1a
bluewhalefz <127460310+bluewhalefz@users.noreply.github.com>
2025-01-14 17:19:42 +0800

    Add files via upload

--- code/FDPSM.py
+++ code/FDPSM.py
@@ -8,11 +8,15 @@ import re
 import itertools as it
 
 from lightgbm import early_stopping
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import roc_auc_score
+from sklearn.model_selection import train_test_split, RandomizedSearchCV,StratifiedKFold
+from sklearn.metrics import roc_auc_score, make_scorer
 from openfe import OpenFE, transform, tree_to_formula
 import lightgbm as lgb
 import warnings
+import os
+from scipy.stats import uniform, randint, loguniform
+import random
+
 
 
 warnings.simplefilter(action='ignore', category=FutureWarning)
@@ -28,7 +32,7 @@ selection_params = {
     'drop_seed':1,
     'max_drop':5,
     'num_leaves': 31,
-    'max_depth':5,     #ver1：max_depth : 6   AUC为0.73是max_depth为5，min_data_in_leaf为51，num_leaves为30
+    'max_depth':5,     #ver1：max_depth : 6   
     'max_bin': 255,
     'min_data_in_leaf': 61,    #ver：min_data_in_leaf = 101
     'learning_rate': 0.01,    # ver1、ver2:'learning_rate': 0.01
@@ -55,7 +59,7 @@ def load_data(df_train, y_train):
         train_x,
         train_y,
         test_size=0.2,
-        random_state=1,
+        random_state=2024,
         stratify=train_y
     )
     lgb_train = lgb.Dataset(X, y)
@@ -87,12 +91,12 @@ def autofe(data, openfe_features, selectedFea_file, importance_file, feaName_fil
     # fit()方法中学到的统计信息，也可以是一些预定义的转换规则。
     X_train_tr, X_test_tr = transform(X_train, X_test, features, n_jobs=30)
     print("**********************finished***********************")
-    # joblib.dump(features, os.path.join(root, f'result/openFE_{filename}.features'))
+   
     joblib.dump(features, openfe_features)
-    #
+    
     X_train_tr.index = list(range(X_train_tr.shape[0]))
     
-    with open(feaNama_file, 'w') as f_write:
+    with open(feaName_file, 'w') as f_write:
         f_write.write('name\n')
         for feature in features:
             f_write.write(tree_to_formula(feature) + '\n')
@@ -100,7 +104,7 @@ def autofe(data, openfe_features, selectedFea_file, importance_file, feaName_fil
     
     feature_num = X_train_tr.shape[1]
     print("feature_num:", feature_num)
-    train_round = 1
+    train_round = 0
 
     while feature_num > 200:
         gbm = train_model(X_train_tr, Y_train)
@@ -126,206 +130,99 @@ def autofe(data, openfe_features, selectedFea_file, importance_file, feaName_fil
     
 
 
-def step_training(data, model_file):
-    X_train = data.iloc[:, 10:]
-    Y_train = data['label']
-    X_train = X_train.astype("float64")
-    lgb_train = lgb.Dataset(X_train, Y_train, free_raw_data=False)
+def step_training(data, model_file, FDPSM_importance):
+    X = data.iloc[:, 10:]
+    Y = data['label']
+    X = X.astype("float64")
+    X_train, X_val, Y_train, Y_val = train_test_split(X, Y, test_size=0.2, random_state=2024)
+     
 
-    # set init params excluding CV params
-    params = {
+    param_dist = {
+    'num_leaves': range(5, 100, 5),
+    'max_depth': range(3, 8, 1),
+    'max_bin': range(5, 256, 10),
+    'min_data_in_leaf': range(1, 102, 10),
+    'learning_rate': [0.001, 0.004, 0.007, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1, 0.2, 0.3, 0.4,0.5],
+    #'learning_rate': np.logspace(-3, 0, 10),  # 在0.001到1之间对数均匀分布
+    'feature_fraction': np.linspace(0.6, 1.0, 5),  # 在0.6到1之间线性均匀分布
+    'bagging_fraction': np.linspace(0.6, 1.0, 5),
+    'bagging_freq': range(0, 50, 5),
+    'lambda_l1': [1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0],
+    'lambda_l2': [1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0],
+    #'lambda_l2': np.logspace(-5, 0, 6),
+    'min_split_gain': np.linspace(0.0, 1.0, 11)
+    }
+
+    fixed_params = {
         'boosting_type': 'gbdt',
         'objective': 'binary',
         'metric': 'auc',
         'nthread': 4,
-        'learning_rate': 0.1
+        'verbose': -1  # 设置为-1以减少输出，或在调试时设置为更高的值
     }
-    max_auc = float('0')
-    best_params = {}
-
-    # Imporve accuracy
-    for num_leaves in range(5, 100, 5):
-        for max_depth in range(3, 8, 1):
-            params['num_leaves'] = num_leaves
-            params['max_depth'] = max_depth
-
-            cv_results = lgb.cv(
-                params,
-                lgb_train,
-                seed=1,
-                nfold=5,
-                metrics=['auc'],
-                callbacks=callbacks,
-                eval_train_metric=True
-            )
-
-            mean_auc = pd.Series(cv_results['valid auc-mean']).max()
-
-            if mean_auc >= max_auc:
-                max_auc = mean_auc
-                best_params['num_leaves'] = num_leaves
-                best_params['max_depth'] = max_depth
-    if 'num_leaves' and 'max_depth' in best_params.keys():
-        params['num_leaves'] = best_params['num_leaves']
-        params['max_depth'] = best_params['max_depth']
-
-    # Avoid over-fitting
-    for max_bin in range(5, 256, 10):
-        for min_data_in_leaf in range(1, 102, 10):
-            params['max_bin'] = max_bin
-            params['min_data_in_leaf'] = min_data_in_leaf
-
-            cv_results = lgb.cv(
-                params,
-                lgb_train,
-                seed=1,
-                nfold=5,
-                metrics=['auc'],
-                callbacks=callbacks,
-                eval_train_metric=True
-            )
-
-            mean_auc = pd.Series(cv_results['valid auc-mean']).max()
-
-            if mean_auc >= max_auc:
-                max_auc = mean_auc
-                best_params['max_bin'] = max_bin
-                best_params['min_data_in_leaf'] = min_data_in_leaf
-    if 'max_bin' and 'min_data_in_leaf' in best_params.keys():
-        params['min_data_in_leaf'] = best_params['min_data_in_leaf']
-        params['max_bin'] = best_params['max_bin']
-
-    for feature_fraction in [0.6, 0.7, 0.8, 0.9, 1.0]:
-        for bagging_fraction in [0.6, 0.7, 0.8, 0.9, 1.0]:
-            for bagging_freq in range(0, 50, 5):
-                params['feature_fraction'] = feature_fraction
-                params['bagging_fraction'] = bagging_fraction
-                params['bagging_freq'] = bagging_freq
-
-                cv_results = lgb.cv(
-                    params,
-                    lgb_train,
-                    seed=1,
-                    nfold=5,
-                    metrics=['auc'],
-                    callbacks=callbacks,
-                    eval_train_metric=True
-                )
-
-                mean_auc = pd.Series(cv_results['valid auc-mean']).max()
-                boost_rounds = pd.Series(cv_results['valid auc-mean']).idxmax()
-
-                if mean_auc >= max_auc:
-                    max_auc = mean_auc
-                    best_params['feature_fraction'] = feature_fraction
-                    best_params['bagging_fraction'] = bagging_fraction
-                    best_params['bagging_freq'] = bagging_freq
-
-    if 'feature_fraction' and 'bagging_fraction' and 'bagging_freq' in best_params.keys():
-        params['feature_fraction'] = best_params['feature_fraction']
-        params['bagging_fraction'] = best_params['bagging_fraction']
-        params['bagging_freq'] = best_params['bagging_freq']
-
-    for lambda_l1 in [1e-5, 1e-3, 1e-1, 0.0, 0.1, 0.3, 0.5, 0.7, 0.9, 1.0]:
-        for lambda_l2 in [1e-5, 1e-3, 1e-1, 0.0, 0.1, 0.4, 0.6, 0.7, 0.9, 1.0]:
-            params['lambda_l1'] = lambda_l1
-            params['lambda_l2'] = lambda_l2
-            cv_results = lgb.cv(
-                params,
-                lgb_train,
-                seed=1,
-                nfold=5,
-                metrics=['auc'],
-                callbacks=callbacks,
-                # verbose_eval=True
-                eval_train_metric=True
-            )
-
-            mean_auc = pd.Series(cv_results['valid auc-mean']).max()
-            boost_rounds = pd.Series(cv_results['valid auc-mean']).idxmax()
-
-            if mean_auc >= max_auc:
-                max_auc = mean_auc
-                best_params['lambda_l1'] = lambda_l1
-                best_params['lambda_l2'] = lambda_l2
-    if 'lambda_l1' and 'lambda_l2' in best_params.keys():
-        params['lambda_l1'] = best_params['lambda_l1']
-        params['lambda_l2'] = best_params['lambda_l2']
-
-    for min_split_gain in [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]:
-        params['min_split_gain'] = min_split_gain
-
-        cv_results = lgb.cv(
-            params,
-            lgb_train,
-            seed=1,
-            nfold=5,
-            metrics=['auc'],
-            callbacks=callbacks,
-            # verbose_eval=True
-            eval_train_metric=True
-        )
-
-        mean_auc = pd.Series(cv_results['valid auc-mean']).max()
-        boost_rounds = pd.Series(cv_results['valid auc-mean']).idxmax()
-
-        if mean_auc >= max_auc:
-            max_auc = mean_auc
-
-            best_params['min_split_gain'] = min_split_gain
-    if 'min_split_gain' in best_params.keys():
-        params['min_split_gain'] = best_params['min_split_gain']
-
-    print("**********params*********")
-    print(params)
+    
+    cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=2024)
+    
+    
+    # 初始化LGBMClassifier
+    lgb_clf = lgb.LGBMClassifier(**fixed_params)
+    # 使用RandomizedSearchCV进行随机搜索和嵌套交叉验证
+    random_search = RandomizedSearchCV(
+        estimator=lgb_clf,
+        param_distributions=param_dist,
+        n_iter=5000,  # 尝试的参数组合数量
+        scoring='roc_auc',  # 使用AUC作为评分指标
+        cv=cv,  # 使用分层交叉验证
+        n_jobs=-1,  # 使用所有可用的CPU核心
+        verbose=1,  # 显示进度
+        random_state=2024,  # 设置随机种子以确保结果可重复
+    )
+    
+    random_search.fit(X, Y)
+    
+    # 输出最优参数
+    print("**********Best Params*********")
+    print(random_search.best_params_)
     print("*************************")
-    lgb_train, lgb_eval = load_data(X_train, Y_train)
-    params = {
-        'boosting_type': 'gbdt',
-        'objective': 'binary',
-        'metric': {'binary_logloss', 'auc'},  # 二进制对数损失
-        'num_leaves': 40,
-        'max_depth': 6,
-        'max_bin': 255,
-        'min_data_in_leaf': 101,
-        'learning_rate': 0.01,    
-        'feature_fraction': 1.0,
-        'bagging_fraction': 1.0,
-        'bagging_freq': 45,
-        'lambda_l1': 0.001,
-        'lambda_l2': 0.4,  # 越小l2正则程度越高
-        'min_split_gain': 0.0,
-        'verbose': 5,
-        'is_unbalance': False
-    }
-    for key in best_params.keys():
-        if key == 'max_depth':
-            params[key] = best_params[key]
-        elif key == 'max_leaves':
-            params[key] = best_params[key]
-        else:
-            params[key] = best_params[key]
-
-    gbm = lgb.train(params,
-                    lgb_train,
-                    num_boost_round=1000,
-                    valid_sets=lgb_eval,
-                    callbacks=callbacks2)
-    joblib.dump(gbm, os.path.join(root, model_file))
-
-
+    print(f"Best AUC: {random_search.best_score_:.4f}")
+    
+    lgb_train, lgb_eval = load_data(X, Y) 
+    
+    # 使用最佳参数训练最终模型
+    best_params = random_search.best_params_
+    final_params = {**fixed_params, **best_params}
+    final_params['metric'] = ['binary_logloss', 'auc']  # 更新为所需的评估指标
+    gbm = lgb.train(
+        final_params,
+        lgb_train,
+        num_boost_round=1000,  # 
+        valid_sets=lgb_eval,
+        callbacks=callbacks2
+        #verbose=True  # 每100次迭代输出一次结果（根据需要调整）
+    )
+    
+    feature_imp = pd.DataFrame({'Value': gbm.feature_importance(), 'Feature': X.columns})
+    feature_sum = feature_imp.Value.sum()
+    df = feature_imp
+    df["importance"] = feature_imp.Value / feature_sum
+    df.to_csv(FDPSM_importance, index=False)
+     
+    # 保存模型
+    joblib.dump(gbm, os.path.join(root, model_file))  # 确保root变量已定义
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--train_file", type=str, help="inputfile: training set")
+    parser.add_argument("--train_file", type=str, help="输入文件")
     parser.add_argument("--selectedFea")
     parser.add_argument("--openfe_features")
     parser.add_argument("--model_file", type=str, help="输出文件")
     parser.add_argument("--importance_file", type=str, help="输出文件")
     parser.add_argument("--feaName_file", type=str, help="输出文件")
+    parser.add_argument("--FDPSM_importance", type=str)
 
     args = parser.parse_args()
     data = pd.read_csv(args.train_file)
-    data = autofe(data, args.openfe_features, args.selectedFea, args.importance_file, args.feaName_file)
-    #step_training(data, args.model_file)
+    #data = autofe(data, args.openfe_features, args.selectedFea, args.importance_file, args.feaName_file)
+    print(data.shape)
+    step_training(data, args.model_file, args.FDPSM_importance)
